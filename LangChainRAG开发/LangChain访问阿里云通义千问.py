3c9b27844c5565e018ce4b2b5940d40c87568c47
syq555666 <2939199532@qq.com>
2026-03-10 11:31:24 +0800

    LangChaindiaoyong

---
+++ LangChainRAG开发/LangChain访问阿里云通义千问.py
@@ -0,0 +1,17 @@
+from langchain_community.llms.tongyi import Tongyi
+import os
+from dotenv import load_dotenv
+
+# 加载.env文件
+load_dotenv()
+
+# 从环境变量获取API Key
+api_key = os.getenv("DASHSCOPE_API_KEY")
+
+#不用qwen3-max，因为qwen3-max是聊天模型，qwen-max是大语言模型
+model = Tongyi(model="qwen-max")
+
+
+res = model.invoke(input="你是谁，能做什么？")
+
+print(res)
